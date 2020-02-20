cbdc45598d805b910a65d84af1d06ffcc4ac284d
Zachary Cardoza <2280384+bayssmekanique@users.noreply.github.com>
2020-02-19 23:33:11 -0800

    Changes npm deploy strategy Publishes from project root

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,23 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const WriteJsonPlugin = require('write-json-webpack-plugin')
-const CreateFileWebpack = require('create-file-webpack')
 const DIST = path.resolve(__dirname, '../dist')
-const package = Object.assign(require('../package.json'), {
-  main: 'index.js',
-  types: 'index.d.ts',
-  private: false,
-  devDependencies: {},
-  scripts: {},
-  files: [
-    './dist/index.js',
-    './dist/index.d.ts',
-    'LICENSE.md',
-    'README.md'
-  ]
-})
 
 function DtsBundlePlugin(options) { this.options = options }
 DtsBundlePlugin.prototype.apply = function (compiler) {
@@ -71,18 +55,6 @@ module.exports = {
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: [DIST],
       cleanAfterEveryBuildPatterns: [path.join(DIST, 'types')]
-    }),
-    // new CopyWebpackPlugin([{ from: 'LICENSE.md', to: 'LICENSE.md' }]),
-    // new CopyWebpackPlugin([{ from: 'README.md', to: 'README.md' }]),
-    // new CreateFileWebpack({
-    //   path: './dist',
-    //   fileName: '.npmrc',
-    //   content: '//registry.npmjs.org/:_authToken=${NPM_TOKEN}'
-    // }),
-    new WriteJsonPlugin({
-      object: package,
-      filename: 'package.json',
-      pretty: true
     }),
     new DtsBundlePlugin({
       name: 'VueClick',
